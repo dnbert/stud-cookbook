5f3a20b6df5fcb4b0a9bdb3824b1ca89e7f1f107
Kyle Bader <kyle.bader@dreamhost.com>
2012-01-03 08:53:11 -0800

    Lint test cleanups
    Used Foodcritic, a lint test suite for chef cookbooks, on the dreamhost
    cookbooks. This commit cleans up violations of FC001, FC003, FC007, FC011
    FC012, FC016, and finally, FC017. You can see the report here:
    
    https://ci.newdream.net/view/DreamHost/job/foodcritic-all/2/console
    
    The Food critic rules are explained here:
    
    http://acrmp.github.com/foodcritic/
    Change-Id: I5ca9ab380ae2e6fdf97d338d1dde1d9c95adda3e
    Reviewed-on: https://git.newdream.net/3692
    Reviewed-by: Kyle Bader <kyle.bader@dreamhost.com>
    Tested-by: Kyle Bader <kyle.bader@dreamhost.com>

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,5 @@ maintainer			"Brett Gailey"
 maintainer_email	"brett.gailey@dreamhost.com"
 license				"Apache 2.0"
 description			"Installs the parted disk utility"
-long_description	IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description▸   IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version				"0.0.1"
